b93b6fa54ce5406e517257a0a1423fc5fefd755c
Ixi <ixioxen251020@gmail.com>
2026-07-25 07:12:37 +0630

    created carpetcalculation function,delete,clear features .

--- carpet.js
+++ carpet.js
@@ -1,5 +1,6 @@
 let currentInput = '';
-let price = '';
+let yardPrice = '';
+let inchPrice = '';
 let showPlus = false;
 let pieces = [];
 
@@ -20,6 +21,8 @@ function renderCarpet(){
 
     if(text === "" && currentInput === ""){
         expression.textContent = "0";
+    }else if(currentInput == '' && text !== ''){
+         expression.textContent = text + '+';
     }
     else if(showPlus){
         expression.textContent = text + " + ";
@@ -31,12 +34,22 @@ function renderCarpet(){
         expression.textContent = currentInput;
     }
 
+    // ui design handling
+    let textLength = expression.textContent.length;
+    if(textLength <= 20 ){
+        expression.style.fontSize = '30px';
+    }else{
+        expression.style.fontSize = '20px';
+    }
 }
 renderCarpet();
 
 // number button
 cNums.forEach(cNum => {
     cNum.addEventListener('click',function(event){
+    if(currentInput == '' && event.target.textContent === '0'){
+        return;
+    }
         currentInput += event.target.textContent;
         showPlus = false;
         renderCarpet();
@@ -48,11 +61,69 @@ addPiece.addEventListener('click',function(){
     if(currentInput === '') return;
     showPlus = true;
     pieces.push(currentInput);
-    currentInput = '';
+    currentInput = '';   
     renderCarpet()
 });
 
+// carpet Dot validatoion
+carpetDot.addEventListener('click',function(){
+    if(currentInput.includes('.')){
+        return;
+    }else{
+        currentInput += '.';
+    }
+    renderCarpet()
+});
+
+// dellete button
+cDel.addEventListener('click',function(){
+    if(currentInput !== ''){
+        currentInput = currentInput.slice(0, -1);
+    }else if(pieces.length > 0){
+        currentInput = pieces.pop();
+        showPlus = false;  
+    }
+    console.log(currentInput);
+    console.log(pieces)
+    
+    renderCarpet()
+});
+
+// clear features 
+cClear.addEventListener('click',function(){
+    pieces = [];
+    currentInput = '';
+    renderCarpet();
+})
+
+// calculation
+function carpetCalculation(){
+    let totalYard = 0;
+    let totalInch = 0;
+    pieces.forEach(pieceItem => {
+        let parts = pieceItem.split('.');
+        let yard = Number(parts[0]);
+        let inch = Number(parts[1] || 0);
+        totalYard += yard;
+        totalInch += inch;
+    });
+    let extraYard = Math.floor(totalInch / 36);
+    let extraInch = totalInch % 36;
+    totalYard += extraYard;
+    totalInch = extraInch;
+    return{
+        totalYard,
+        totalInch
+    };
+};
+
+// 3600 button
+// cOperators.forEach(cOperator => {
+//     cOperator.addEventListener('click',function(event){
+//         if(event.target.textContent == 3600 ){
+            
 
 
-
-
+//         }
+//     })
+// })
